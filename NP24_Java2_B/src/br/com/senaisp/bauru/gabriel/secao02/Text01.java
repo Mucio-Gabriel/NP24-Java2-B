77766da2ba1c1cc42327970c4cf5d1fbbcd32e63
Múcio-Gabriel <gabrielmucio7474@gmail.com>
2024-03-05 22:02:35 -0300

    Conteúdo do dia 05-03-24

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/gabriel/secao02/Text01.java
@@ -0,0 +1,22 @@
+package br.com.senaisp.bauru.gabriel.secao02;
+
+public class Text01 {
+	/**
+	 * Comentario de documentação
+	 * @param args
+	 */
+    public static void main(String[] args) {
+    //Cometario de uma linha
+    /*
+     * Comentario de mais de uma linha
+     */
+    	System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+}
