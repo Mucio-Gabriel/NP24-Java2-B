fe25f9934921b9438e5e21a768785be5d958783c
Múcio-Gabriel <gabrielmucio7474@gmail.com>
2024-04-03 21:58:17 -0300

    Conteudo da Aula 03-04-24

--- Projeto_BancoDados/src/br/com/senaisp/bauru/gabriel/classes/ProdutoSimples.java
+++ Projeto_BancoDados/src/br/com/senaisp/bauru/gabriel/classes/ProdutoSimples.java
@@ -1,6 +1,5 @@
 package br.com.senaisp.bauru.gabriel.classes;
 
-import java.awt.Taskbar.State;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,39 +8,39 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ProdutoSimples {
-	
-	private int  id;
+
+	private int id;
 	private String descricao;
 	private int saldo;
 	private double custo;
-	
-	//Usando o conector para BD(Banco de Dados)
-	
+
+	// Usando o conector para BD(Banco de Dados)
+
 	private Connection conn;
-	
-	//Constructor 
-	
+
+	// Constructor
+
 	public ProdutoSimples() throws SQLException {
-	
-	id = 0;
-	saldo = 0;
-	custo = 0;
-	
-	//Instanciando o DriverManager
-	conn = DriverManager.getConnection("jdbc:sqlite:C:\\Javalibs\\dados\\NP24_JAVA2_B.DB");
-		
+
+		id = 0;
+		saldo = 0;
+		custo = 0;
+
+		// Instanciando o DriverManager
+		conn = DriverManager.getConnection("jdbc:sqlite:C:\\Javalibs\\dados\\NP24_JAVA2_B.DB");
+
 	}
 
-	//Metodos (Getters and Setters)
-	
+	// Metodos (Getters and Setters)
+
 	public int getId() {
 		return id;
 	}
 
 	public void setId(int id) throws Exception {
-		
-		if(id<0) {
-			throw new Exception ("O id não pode ser negativo");
+
+		if (id < 0) {
+			throw new Exception("O id não pode ser negativo");
 		}
 		this.id = id;
 	}
@@ -69,37 +68,104 @@ public class ProdutoSimples {
 	public void setCusto(double custo) {
 		this.custo = custo;
 	}
+
+	//
+
+	public String toString() {
+		return "Id: " + getId() + "\n" + "Descrição: " + getDescricao() + "\n" + "Saldo: " + getSaldo() + "\n"
+				+ "Custo :" + getCusto();
+	}
+
+	//
+	public void gravar() throws SQLException {
+		PreparedStatement stmt = conn.prepareStatement("update produto set descricao=?, saldo=?, custo=? where id=?");
+	
+		// Preenchendo os Paramentos
+		stmt.setString(1, getDescricao());
+		stmt.setInt(2, getSaldo());
+		stmt.setDouble(3, getCusto());
+		stmt.setInt(4, getId());
+		
+		//Disparado a query
+		int nRegs = stmt.executeUpdate();
+		System.out.println(nRegs + "Registro(s) afetado(s)!");
+		
+	}
+	public void apagar() throws SQLException {
+		PreparedStatement stmt = conn.prepareStatement("delete from produto where id=?");
+		
+		// Preenchendo os Paramentos
+		stmt.setInt(1, getId());
+		
+		//Disparado a query
+		int nRegs = stmt.executeUpdate();
+		System.out.println(nRegs + "Registro(s) afetado(s)!");
+		
+	}
+
 	//
 	public static ProdutoSimples create(String des, int sal, double cus) throws SQLException {
+
 		ProdutoSimples ret = new ProdutoSimples();
 		ret.setDescricao(des);
 		ret.setCusto(cus);
 		ret.setSaldo(sal);
-		
-		//Gravar no banco de dados
-		
-		//Preparando o comando para o banco de dados
-		PreparedStatement stm = ret.conn.prepareStatement("insert into produto(descricao,saldo,custo) values"
-															+ "values(?,?,?)",Statement.RETURN_GENERATED_KEYS);
-		
-		//Os paramentos comecao com 1 e nao zero
+
+		// Gravar no banco de dados
+
+		// Preparando o comando para o banco de dados
+		PreparedStatement stm = ret.conn.prepareStatement(
+				"insert into produto(descricao,saldo,custo) " + "values (?,?,?)", Statement.RETURN_GENERATED_KEYS);
+
+		// Os paramentos comecao com 1 e nao zero
 		stm.setString(1, des);
 		stm.setInt(2, sal);
 		stm.setDouble(3, cus);
-		
-		//Executando o
+
+		// Executando o
 		int linhas = stm.executeUpdate();
-		System.out.println("Adicionei " + linhas + " no banco");
+		System.out.println("Adicionei " + linhas + " no banco!");
 		ResultSet rs = stm.getGeneratedKeys();
 		rs.next();
 		try {
 			ret.setId(rs.getInt(1));
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		//
 		return ret;
 	}
-	
+
+	public static ProdutoSimples FindByPK(int cod) throws Exception {
+		ProdutoSimples ret = new ProdutoSimples();
+		if (cod > 0) {
+			// Preparando istrução de select
+			PreparedStatement stmt = ret.conn
+					.prepareStatement("select id, descricao ,saldo ,custo " + " from produto where id = ?");
+
+			// Colocando o parametro da query
+			stmt.setInt(1, cod);
+
+			// Executando a query
+			ResultSet rs = stmt.executeQuery();
+
+			// Verificando se encontrou algo
+			if (rs.next()) {
+				ret.setId(rs.getInt(1));
+				ret.setDescricao(rs.getString(2));
+				ret.setSaldo(rs.getInt(3));
+				ret.setCusto(rs.getDouble(4));
+
+			} else {
+				throw new Exception("Reegistro não encontrado!");
+			}
+
+		} else {
+			throw new Exception("O codigo deve ser mairo que zero!");
+		}
+
+		return ret;
+	}
+
 }
--- Projeto_BancoDados/src/br/com/senaisp/bauru/gabriel/testers/TesteProdutoSimples.java
+++ Projeto_BancoDados/src/br/com/senaisp/bauru/gabriel/testers/TesteProdutoSimples.java
@@ -1,29 +1,90 @@
 package br.com.senaisp.bauru.gabriel.testers;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import br.com.senaisp.bauru.gabriel.classes.ProdutoSimples;
 
 public class TesteProdutoSimples {
 
 	public static void main(String[] args) {
-		/*
-		ProdutoSimples prd = new ProdutoSimples();
+		Scanner sc = new Scanner(System.in);
+		int op;
+		do {
+			System.out.println("Menu");
+			System.out.println("1 - Criar Produto");
+			System.out.println("2 - Consultar Produto");
+			System.out.println("3 - Alterar Produto");
+			System.out.println("4 - Excluir Produto");
+			System.out.println("9 - Fim");
+			op = sc.nextInt();
+			switch (op) {
+			case 1 -> criarProduto(sc);
+			case 2 -> consultarProduto(sc);
+			case 3 -> alterarProduto(sc);
+			case 4 -> excluirProduto(sc);
+			case 9 -> System.out.println("Bye!");
+			default -> System.out.println("Opção invalida!");
+			}
+		} while (op != 9);
+	}
+
+	private static void excluirProduto(Scanner sc) {
+		// TODO Auto-generated method stub
+	}
+
+	private static void alterarProduto(Scanner sc) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void consultarProduto(Scanner sc) {
+		sc.nextLine();
+		System.out.println("Consulta de produtos");
+		System.out.println("Digite o codigo do produto a pesquisar:");
+		int cod = sc.nextInt();
 		try {
-			prd.setId(-10);
+			// Pesquisando o produto
+			ProdutoSimples prd = ProdutoSimples.FindByPK(cod);
+			// Mostranfdo o produto
+			System.out.println(prd);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		System.out.println(prd.getId());
-	
-	*/
-	
-		try {
-			ProdutoSimples prd = ProdutoSimples.create("Lapis", 20, 1.20);
-			System.out.println(prd.getId());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}	
+
+		// Pausa para voltar para o menu
+		System.out.println("Digite algo para voltar ao menu");
+		sc.nextLine(); // Captura do ultimo enter numetico
+		sc.nextLine(); // Pauda para voltar
+
 	}
 
+	private static void criarProduto(Scanner sc) {
+		sc.nextLine();// Captura do enter do menu
+
+		System.out.println("Cadastramento de produto:");
+
+		System.out.println("Digite a descriçaõ do produto:");
+		String desc = sc.nextLine();
+
+		System.out.println("Digite o saldo do produto:");
+		int sal = sc.nextInt();
+
+		System.out.println("Digite ocusto do produto:");
+		double cus = sc.nextDouble();
+
+		try {
+			// Criando o produto
+			ProdutoSimples prd = ProdutoSimples.create(desc, sal, cus);
+			// Mostrando o produto
+			System.out.println(prd);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+
+		// Pausa para voltar ao menu
+		System.out.println("Digite enter para voltsr ao menu");
+		sc.nextLine();
+	}
 }
